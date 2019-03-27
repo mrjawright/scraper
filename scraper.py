7473b947e792c874404edb2f395c707521f95cff
Ubuntu <ubuntu@ip-172-31-94-219.ec2.internal>
2019-03-27 14:55:29 +0000

    implemented feedparser

--- scraper.py
+++ scraper.py
@@ -1,37 +1,30 @@
 #!/usr/bin/python
+import feedparser
 import sys
-import time
-import urllib2
-from urllib2 import urlopen
-import re
-import cookielib, urllib2
-from cookielib import CookieJar
-import datetime
-cj = CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+
+class WhizRssAggregator():
+    feedurl = ""
+
+    def __init__(self, subreddit):
+        self.feedurl = 'https://reddit.com/r/'+subreddit+".rss"
+        self.parse()
+    def parse(self):
+        thefeed = feedparser.parse(self.feedurl)
+        print("Getting Feed Data")
+       	print("__________")
+	for thefeedentry in thefeed.entries:
+            	print(thefeedentry.get("id", ""))
+            	print(thefeedentry.get("title", ""))
+            	print(thefeedentry.get("link", ""))
+        	print(thefeedentry.get("author", ""))
+            	print(thefeedentry.get("content", ""))
+            	print("__________")
 
 def main():
     try:
         if len(sys.argv) == 2:
-        	page = 'https://reddit.com/r/'+sys.argv[1]+".rss"
-        	sourceCode = opener.open(page).read()
-        	try:
-            		titles = re.findall(r"<title>(.*?)</title>",sourceCode)
-            		contents = re.findall(r"<content>(.*?)</content>",sourceCode)
-            		ids = re.findall(r"<id>(.*?)</id>",sourceCode)
-            		authors = re.findall(r"<author>(.*?)</author>",sourceCode)
-            		for title  in titles:
-                		print title
-            		for content  in contents:
-                		print content
-            		for id in ids:
-                		print id
- 			for author in authors:
-				name = re.search("<name>(.*?)</name>",author)
-                                print author[(name.start() + 6): (name.end() - 7):]  
-        	except Exception, e:
-            		print str(e)
+#                from whizrssaggregator import WhizRssAggregator
+		rssobject = WhizRssAggregator(sys.argv[1])				 
 	else:
 		print sys.argv[0] + " subredditname"	
     except Exception,e:
