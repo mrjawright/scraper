96ab3ddafed13b1a977fc110ebae1f4e875dca62
mrjawright <mr.jawright@gmail.com>
2019-04-04 20:19:37 +0000

    added test that mocks the fetchfeed function refactored the code into  smaller functions

--- scraper.py
+++ scraper.py
@@ -8,106 +8,129 @@ import os
 
 class WhizRssAggregator():
 	
-	def debug(self, value = None):
-		if not value is None:
-			self.debug = value
-		return self.debug
-
 	def print(self, *args, **kwargs):
 		if self.debug:
 			return __builtin__.print(*args, **kwargs)
 
+	def debug(self, value = None):
+		if not value is None:
+			self.debug = value
+		self.print("Debug is %s" % self.debug)
+		return self.debug
+
+	def testing(self, value = None):
+		if not value is None:
+			self.testing = value
+		self.print("Testing is %s" % self.testing)
+		return self.testing
+
 	def __init__(self, cachedir, subreddit):
 		try:
 			self.print("WhizRssAggregator cacheing %s images to %s" % (subreddit, cachedir))
-			self.cachedir = cachedir
-			self.pwd = os.path.dirname(os.path.abspath(__file__))
+			pwd = os.path.dirname(os.path.abspath(__file__))
 			#self.print ("PWD: %s" % self.pwd)
-			path = os.path.join(self.pwd, self.cachedir)
-			self.print ("Cache path: %s" % path)
-			if os.path.isdir(path):
-				self.print("%s already exists" % path)
+			self.cachedir = os.path.join(pwd, cachedir)
+			self.print ("Cache path: %s" % self.cachedir)
+			if os.path.isdir(self.cachedir):
+				self.print("%s already exists" % self.cachedir)
 			else:
-    				os.mkdir(path)
-    				self.print ("Successfully created the directory %s " %path)
+    				os.mkdir(self.cachedir)
+    				self.print ("Successfully created the directory %s " % self.cachedir)
 		except OSError:  
 			print("OS error: {0}".format(err)) 
 	
 		if subreddit != "":
 			self.subreddit = subreddit
-			self.feedurl = 'https://reddit.com/r/'+subreddit+".rss"
 		else:
 			raise RuntimeError("Null or Empty subreddit name")
 
 	def fetchfeed(self):
 		print("Getting Feed Data for %s " % self.subreddit)
-		thefeed = feedparser.parse(self.feedurl)
+		feedurl = 'https://reddit.com/r/'+subreddit+".rss"
+		if self.debug and not self.testing:
+			urllib.request.urlretrieve(feedurl,self.subreddit+".feed")
+		thefeed = feedparser.parse(feedurl)
 		self.print(thefeed['feed'])
 		return thefeed
 
-	def parsefeed(self):
-		thefeed = self.fetchfeed()
+	def parsefeed(self, thefeed):
 		self.print(thefeed['feed']['title'])
 		try:
 			self.print(thefeed.feed.subtitle)
 		except:	
-			self.print("/n")	
-		print("%s Entries" % len(thefeed['entries']))
-		return thefeed
+			self.print("/n")
+		self.entries = len(thefeed['entries'])
+		print("%s Entries" % self.entries)
+		return thefeed['entries']
 
-	def parseentries(self, thefeed):
+	def getfilefromcontent(self, content):
+		file = None
+		link_href = None
 		link_regex = r"<span><a href=\"((http[s]?):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?\">\[link\]<\/a><\/span>"
+		link = re.search(link_regex, content)
+		if not link == None:
+			if (link.start()+15 < link.end()-19): 
+				tlink_href = content[link.start()+15:link.end()-19]
+				self.print(tlink_href)
+				href_split = re.split('\/',tlink_href)
+				subreddit_path = os.path.join(self.cachedir ,self.subreddit)
+				tfile = os.path.join(subreddit_path,href_split[len(href_split)-1])
+				if tfile.endswith(".jpg") and not os.path.exists(tfile):
+					try:
+						file = tfile
+						link_href = tlink_href
+						#if there are images to fetch, check to see if we've set up the
+						#folder for the current subreddit
+						if self.testfordir:
+							#there's no use checking in every iteration of the loop
+							if os.path.isdir(subreddit_path):
+								print("%s already exists" % subreddit_path)
+							else:
+    								os.mkdir(subreddit_path)
+    								print ("Successfully created the directory %s " % subreddit_path)
+							self.testfordir = False
+					except OSError as err: 
+						print("OS error: %s" % format(err)) 
+						print("** error: %s" % err.errno) 
+						print("** file: %s" % err.filename) 
+					except:	
+						print("Unexpected Error:", sys.exc_info()[0])
+		return link_href, file
+
+	def parsecontent(self, content):
+		link_href, file = self.getfilefromcontent(content['value'])
+		if not file == None:
+			try:
+				urllib.request.urlretrieve(link_href,file)
+			except urllib.error.HTTPError as http_err:
+				print(http_err)
+				print("While processing: %s" % link_href)
+
+	def parseentries(self, theentries):
+		self.testfordir=True
 		self.print("__________")
-		testfordir=True
-		for thefeedentry in thefeed.entries:
-			self.print(thefeedentry['id'])
-			self.print(thefeedentry['title'])
-			self.print(thefeedentry['tags'])
-			self.print(thefeedentry['link'])
-			self.print(thefeedentry['author'])
-			self.print(thefeedentry['summary'])
-			for content in thefeedentry['content']:
-				link = re.search(link_regex, content['value'])
-				if not link == None:
-					if (link.start()+15 < link.end()-19): 
-						link_href = content['value'][link.start()+15:link.end()-19]
-						self.print(link_href)
-						href_split = re.split('\/',link_href)
-						subreddit_path = os.path.join(self.pwd, self.cachedir ,self.subreddit)
-						file = os.path.join(subreddit_path,href_split[len(href_split)-1])
-						if file.endswith(".jpg") and not os.path.exists(file):
-							try:
-								#if there are images to fetch, check to see if we've set up the
-								#folder for the current subreddit
-								if testfordir:
-								#there's no use checking in every iteration of the loop
-									if os.path.isdir(subreddit_path):
-										print("%s already exists" % subreddit_path)
-									else:
-    										os.mkdir(subreddit_path)
-    										print ("Successfully created the directory %s " % subreddit_path)
-									testfordir = False
-								urllib.request.urlretrieve(link_href,file)
-							except urllib.error.HTTPError as http_err:
-								print(http_err)
-								print("Processing: %s" % link_href)
-							except OSError as err: 
-								print("OS error: %s" % format(err)) 
-								print("** error: %s" % err.errno) 
-								print("** file: %s" % err.filename) 
-							except:	
-								print("Unexpected Error:", sys.exc_info()[0])
-				self.print("__________")
+		for entry in theentries:
+			self.print(entry['id'])
+			self.print(entry['title'])
+			self.print(entry['tags'])
+			self.print(entry['link'])
+			self.print(entry['author'])
+			self.print(entry['summary'])
+			for content in entry['content']:
+				self.parsecontent(content)
+		self.print("__________")
 
 	def parse(self):
-		thefeed = self.parsefeed()
-		self.parseentries(thefeed)
+		thefeed = self.fetchfeed()
+		theentries = self.parsefeed(thefeed)
+		self.parseentries(theentries)
 
 def main():
 	try:
 		if len(sys.argv) == 2:
 			rssobject = WhizRssAggregator('imagecache', sys.argv[1])
 			rssobject.debug(False)
+			rssobject.testing(False)
 			rssobject.parse()
 		else:
 			print ("usage:"+ sys.argv[0] + " subredditname")
--- tests.py
+++ tests.py
@@ -2,42 +2,42 @@ import io
 import sys
 import os
 from contextlib import redirect_stdout
-from scraper import WhizRssAggregator 
 import shutil
 import unittest
+import feedparser
 from unittest import mock 
+from unittest.mock import MagicMock
+from scraper import WhizRssAggregator 
 
 def setUpModule():
-	print("Set up module")
+	print("\nSet up module")
 
 def tearDownModule():
-	print("Tear down module")
+	print("\nTear down module")
 
 
 class TestParse(unittest.TestCase):
 	cachedir = "test"
+	pwd = os.path.dirname(os.path.abspath(__file__))
+	cache_path = os.path.join(pwd, cachedir)
 
 	@classmethod
 	def setUpClass(cls):
-		print("Set up TestParseClass")
-		pwd = os.path.dirname(os.path.abspath(__file__))
-		path = os.path.join(pwd, cls.cachedir)
-		if os.path.isdir(path):
-			shutil.rmtree(path)
+		print("\nSet up TestParseClass")
+		if os.path.isdir(cls.cache_path):
+			shutil.rmtree(cls.cache_path)
 
 	@classmethod
 	def tearDownClass(cls):
-		print("Tear down TestParseClass")
-		pwd = os.path.dirname(os.path.abspath(__file__))
-		path = os.path.join(pwd, cls.cachedir)
-		if os.path.isdir(path):
-			shutil.rmtree(path)
+		print("\nTear down TestParseClass")
+		if os.path.isdir(cls.cache_path):
+			shutil.rmtree(cls.cache_path)
 	
 	def setUp(self):
-		print("Set up")
+		print("\nSet up")
 
 	def tearDown(self):
-		print("Tear down")
+		print("\nTear down")
 
 	def test_parse_none(self):
 		print("test_parse_none")
@@ -50,11 +50,46 @@ class TestParse(unittest.TestCase):
 			rssobject = WhizRssAggregator(self.cachedir,"test")
 			self.assertNotEqual(buf.getvalue(),"")
 
-	@mock.patch('scraper.WhizRssAggregator')
-	def test_MockScraper(mock_class, cachedir, subreddit ):
-		mock_class.return_value.fetchfeed.returnvalue = r"{'tags': [{'term': 'EarthPorn', 'scheme': None, 'label': 'r/EarthPorn'}], 'updated': '2019-04-03T15:28:08+00:00', 'updated_parsed': time.struct_time(tm_year=2019, tm_mon=4, tm_mday=3, tm_hour=15, tm_min=28, tm_sec=8, tm_wday=2, tm_yday=93, tm_isdst=0), 'icon': 'https://www.redditstatic.com/icon.png/', 'id': 'https://www.reddit.com/r/earthporn.rss', 'guidislink': True, 'link': 'https://www.reddit.com/r/earthporn', 'links': [{'rel': 'self', 'href': 'https://www.reddit.com/r/earthporn.rss', 'type': 'application/atom+xml'}, {'rel': 'alternate', 'href': 'https://www.reddit.com/r/earthporn', 'type': 'text/html'}], 'logo': 'https://b.thumbs.redditmedia.com/ksUDkpsi-29HAGUhp7k-qq2029z3FqC4fad686B4jGE.png', 'subtitle': 'EarthPorn is your community of landscape photographers and those who appreciate the natural beauty of our home planet.', 'subtitle_detail': {'type': 'text/plain', 'language': None, 'base': 'https://www.reddit.com/r/earthporn.rss', 'value': 'EarthPorn is your community of landscape photographers and those who appreciate the natural beauty of our home planet.'}, 'title': 'EarthPorn: Amazing images of light and landscape', 'title_detail': {'type': 'text/plain', 'language': None, 'base': 'https://www.reddit.com/r/earthporn.rss', 'value': 'EarthPorn: Amazing images of light and landscape'}}"
-		mock_class.return_value.__init__ = lambda testclass, cachedir, subreddit:None
-		inst = scraper.WhizRssAggregator(cachedir = self.cachedir,subreddit = "mock")
+#	def test_init_mock(self):
+#		print("test_init_mock")
+#		img_path = os.path.join(self.cache_path, "earthporn")
+#		with mock.patch.object(WhizRssAggregator, "__init__", lambda obj, dir, sub: None):
+#			test_obj = WhizRssAggregator(self.cachedir, "spaceporn")
+#			#create attributes from __init__
+#			test_obj.pwd = self.pwd
+#			#test_obj.cachedir = self.cachedir
+#			#test_obj.subreddit = "spaceporn"
+#			test_obj.debug(True)
+#			self.assertTrue(os.path.isfile(os.path.join(self.pwd,'sample.feed')))
+#			testfeed = feedparser.parse(os.path.join(self.pwd,'sample.feed'))
+#			self.assertFalse(testfeed['feed'] == {})
+#			test_obj.fetchfeed = MagicMock(return_value = testfeed)
+#			test_obj.parse()
+#			self.assertTrue(test_obj.entries==25)		
+#		#self.assertTrue(os.path.isdir(img_path))
+#		self.assertTrue(test_obj.fetchfeed.called)		
+
+	@mock.patch('scraper.WhizRssAggregator.fetchfeed')
+	def test_parse_with_mock(self, fetchfeed_mock):
+		print("test_parse_with_mock")
+		self.assertTrue(os.path.isfile(os.path.join(self.pwd,'sample.feed')))
+		testfeed = feedparser.parse(os.path.join(self.pwd,'sample.feed'))
+		self.assertFalse(testfeed['feed'] == {})
+		
+		test_obj = WhizRssAggregator(self.cachedir, "spaceporn")
+		test_obj.debug(True)
+		fetchfeed_mock.return_value=testfeed
+		test_obj.parse()
+		self.assertTrue(test_obj.entries==25)		
+		self.assertTrue(fetchfeed_mock.called)		
+
+
+
+#	@mock.patch('scraper.WhizRssAggregator')
+#	def test_MockScraper(mock_class, cachedir, subreddit ):
+#		mock_class.return_value.fetchfeed.returnvalue = r"{'tags': [{'term': 'EarthPorn', 'scheme': None, 'label': 'r/EarthPorn'}], 'updated': '2019-04-03T15:28:08+00:00', 'updated_parsed': time.struct_time(tm_year=2019, tm_mon=4, tm_mday=3, tm_hour=15, tm_min=28, tm_sec=8, tm_wday=2, tm_yday=93, tm_isdst=0), 'icon': 'https://www.redditstatic.com/icon.png/', 'id': 'https://www.reddit.com/r/earthporn.rss', 'guidislink': True, 'link': 'https://www.reddit.com/r/earthporn', 'links': [{'rel': 'self', 'href': 'https://www.reddit.com/r/earthporn.rss', 'type': 'application/atom+xml'}, {'rel': 'alternate', 'href': 'https://www.reddit.com/r/earthporn', 'type': 'text/html'}], 'logo': 'https://b.thumbs.redditmedia.com/ksUDkpsi-29HAGUhp7k-qq2029z3FqC4fad686B4jGE.png', 'subtitle': 'EarthPorn is your community of landscape photographers and those who appreciate the natural beauty of our home planet.', 'subtitle_detail': {'type': 'text/plain', 'language': None, 'base': 'https://www.reddit.com/r/earthporn.rss', 'value': 'EarthPorn is your community of landscape photographers and those who appreciate the natural beauty of our home planet.'}, 'title': 'EarthPorn: Amazing images of light and landscape', 'title_detail': {'type': 'text/plain', 'language': None, 'base': 'https://www.reddit.com/r/earthporn.rss', 'value': 'EarthPorn: Amazing images of light and landscape'}}"
+#		mock_class.return_value.__init__ = lambda testclass, cachedir, subreddit:None
+#		inst = scraper.WhizRssAggregator(cachedir = self.cachedir,subreddit = "mock")
 		#inst.subreddit='mock'
 		#inst.parse()
 
