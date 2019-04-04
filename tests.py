e3f104d9e5d1ee4600e062bc2ad72a45fe308556
mrjawright <mr.jawright@gmail.com>
2019-04-04 20:56:22 +0000

    minor changes. added sample.feed file for testing

--- tests.py
+++ tests.py
@@ -50,24 +50,6 @@ class TestParse(unittest.TestCase):
 			rssobject = WhizRssAggregator(self.cachedir,"test")
 			self.assertNotEqual(buf.getvalue(),"")
 
-#	def test_init_mock(self):
-#		print("test_init_mock")
-#		img_path = os.path.join(self.cache_path, "earthporn")
-#		with mock.patch.object(WhizRssAggregator, "__init__", lambda obj, dir, sub: None):
-#			test_obj = WhizRssAggregator(self.cachedir, "spaceporn")
-#			#create attributes from __init__
-#			test_obj.pwd = self.pwd
-#			#test_obj.cachedir = self.cachedir
-#			#test_obj.subreddit = "spaceporn"
-#			test_obj.debug(True)
-#			self.assertTrue(os.path.isfile(os.path.join(self.pwd,'sample.feed')))
-#			testfeed = feedparser.parse(os.path.join(self.pwd,'sample.feed'))
-#			self.assertFalse(testfeed['feed'] == {})
-#			test_obj.fetchfeed = MagicMock(return_value = testfeed)
-#			test_obj.parse()
-#			self.assertTrue(test_obj.entries==25)		
-#		#self.assertTrue(os.path.isdir(img_path))
-#		self.assertTrue(test_obj.fetchfeed.called)		
 
 	@mock.patch('scraper.WhizRssAggregator.fetchfeed')
 	def test_parse_with_mock(self, fetchfeed_mock):
@@ -83,15 +65,6 @@ class TestParse(unittest.TestCase):
 		self.assertTrue(test_obj.entries==25)		
 		self.assertTrue(fetchfeed_mock.called)		
 
-
-
-#	@mock.patch('scraper.WhizRssAggregator')
-#	def test_MockScraper(mock_class, cachedir, subreddit ):
-#		mock_class.return_value.fetchfeed.returnvalue = r"{'tags': [{'term': 'EarthPorn', 'scheme': None, 'label': 'r/EarthPorn'}], 'updated': '2019-04-03T15:28:08+00:00', 'updated_parsed': time.struct_time(tm_year=2019, tm_mon=4, tm_mday=3, tm_hour=15, tm_min=28, tm_sec=8, tm_wday=2, tm_yday=93, tm_isdst=0), 'icon': 'https://www.redditstatic.com/icon.png/', 'id': 'https://www.reddit.com/r/earthporn.rss', 'guidislink': True, 'link': 'https://www.reddit.com/r/earthporn', 'links': [{'rel': 'self', 'href': 'https://www.reddit.com/r/earthporn.rss', 'type': 'application/atom+xml'}, {'rel': 'alternate', 'href': 'https://www.reddit.com/r/earthporn', 'type': 'text/html'}], 'logo': 'https://b.thumbs.redditmedia.com/ksUDkpsi-29HAGUhp7k-qq2029z3FqC4fad686B4jGE.png', 'subtitle': 'EarthPorn is your community of landscape photographers and those who appreciate the natural beauty of our home planet.', 'subtitle_detail': {'type': 'text/plain', 'language': None, 'base': 'https://www.reddit.com/r/earthporn.rss', 'value': 'EarthPorn is your community of landscape photographers and those who appreciate the natural beauty of our home planet.'}, 'title': 'EarthPorn: Amazing images of light and landscape', 'title_detail': {'type': 'text/plain', 'language': None, 'base': 'https://www.reddit.com/r/earthporn.rss', 'value': 'EarthPorn: Amazing images of light and landscape'}}"
-#		mock_class.return_value.__init__ = lambda testclass, cachedir, subreddit:None
-#		inst = scraper.WhizRssAggregator(cachedir = self.cachedir,subreddit = "mock")
-		#inst.subreddit='mock'
-		#inst.parse()
 
 if __name__ == "__main__":
 	unittest.main()	
